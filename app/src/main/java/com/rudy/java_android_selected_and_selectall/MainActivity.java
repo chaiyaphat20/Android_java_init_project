61143159deec285cd1bd348e69cfbc670ccc4df4
chaipat supharak <chaipat.sup@cpf.co.th>
2023-11-06 12:00:22 +0700

    first commit

---
+++ app/src/main/java/com/rudy/java_android_selected_and_selectall/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rudy.java_android_selected_and_selectall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
